ca5980d64ed4b8812976b777c89b14299136ef00
xvyang <1652862811@qq.com>
2025-01-02 21:44:38 +0800

    wfl

--- src/com/shop/service/impl/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Service层，存放实现类
- * 
- *
- */
-package com.shop.service.impl;
---
+++ src/com/shop/web/UI/ManageItemsUISevlet.java
@@ -0,0 +1,13 @@
+package com.shop.web.UI;
+
+/**
+ * 为管理员提供管理商品的UIServlet
+ * 
+ * RegisterUIServlet负责为用户输出注册界面
+ * 
+ *
+ */
+
+public class ManageItemsUISevlet {
+	
+}
